4ab9cd1e397ffc3a20bad288d727dfd8c3feeab0
Dariusz Hozer <33494247+dar23@users.noreply.github.com>
2023-05-12 09:01:20 +0200

    Add files via upload

---
+++ NBA/scripts.js
@@ -0,0 +1,220 @@
+
+
+
+function in_button_left(){
+
+    var x=document.getElementById('stars');
+    var z=document.getElementById('menu_click_right');
+    
+    
+    if(x.style.left="100%"){
+    
+        x.style.animation="star 0.15s forwards";
+       z.style.display="block";
+        }
+        
+    }
+    
+    function in_button_right(){
+    
+        var x=document.getElementById('stars');
+        var y=document.getElementById('menu_click_left');
+        var z=document.getElementById('menu_click_right');
+        
+        
+        if(x.style.left="87%"){
+        
+            x.style.animation="star1 0.1s forwards";
+            z.style.display="none";
+        
+            }
+            
+            
+    
+        }
+        
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function straight_one(){
+
+    var x=document.getElementById('straight1');
+   
+
+    x.style.animation="snake0 2s forwards";
+    x.style.display="block";
+}
+
+setTimeout(straight_one,300);
+
+
+function straight_two(){
+
+    var x=document.getElementById('straight2');
+   
+
+    x.style.animation="snake1 1s  forwards";
+    x.style.display="block";
+ 
+}
+setTimeout(straight_two,1400);
+
+
+function straight_three(){
+
+    var x=document.getElementById('straight3');
+   
+
+    x.style.animation="snake3 1s  forwards";
+    x.style.display="block";
+ 
+}
+
+setTimeout(straight_three,200);
+
+
+
+
+
+
+function straight_four(){
+
+    var x=document.getElementById('straight4');
+   
+
+    x.style.animation="snake4 2s  forwards";
+    x.style.display="block";
+ 
+}
+
+setTimeout(straight_four,200);
+
+
+
+
+
+
+
+function straight_five(){
+
+    var x=document.getElementById('straight5');
+   
+
+    x.style.animation="snake5 2s  forwards";
+    x.style.display="block";
+ 
+}
+
+setTimeout(straight_five,200);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function all_stars(){
+
+    var x=document.getElementById('all_star');
+   
+
+    x.style.animation="light 2s forwards";
+    x.style.display="block";
+}
+
+setTimeout(all_stars,900);
+
+
+
+
+function datas(){
+
+    var x=document.getElementById('data');
+   
+
+    x.style.animation="light 1s forwards";
+    x.style.display="block";
+}
+
+setTimeout(datas,1900);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
